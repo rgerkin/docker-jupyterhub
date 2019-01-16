c487a31f22069c5220576a183b26cc87f4efcd17
Richard Gerkin <rgerkin@asu.edu>
2019-01-16 15:09:38 -0700

    Added SSL and fixed loading of settings

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,20 +1,34 @@
 # jupyterhub_config.py
 c = get_config()
 
-# Change from JupyterHub to JupyterLab
+import os
+pjoin = os.path.join
+
+runtime_dir = os.path.join('/etc/jupyterhub')
+
+# Change from Jupyter Notebook to JupyterLab
 c.Spawner.default_url = '/lab'
 c.Spawner.debug = True
 
+c.JupyterHub.ssl_key = pjoin(runtime_dir,'my.key')
+c.JupyterHub.ssl_cert = pjoin(runtime_dir,'my.cert')
+c.JupyterHub.cookie_secret_file = pjoin(runtime_dir, 'cookie_secret')
+c.JupyterHub.db_url = pjoin(runtime_dir, 'jupyterhub.sqlite')
+c.JupyterHub.extra_log_file = pjoin(runtime_dir, 'jupyterhub.log')
+
 # Administrators - set of users who can administer the Hub itself
-#
 from oauthenticator.github import LocalGitHubOAuthenticator
 c.JupyterHub.authenticator_class = LocalGitHubOAuthenticator
 a = c.LocalGitHubOAuthenticator
 
-from .jupyterhub_config_private import url, port, client_id, client_secret, admin_users, white_list
-a.oauth_callback_url = '%s:%d/hub/oauth_callback' % (url, port)
-a.client_id = client_id
-a.client_secret = client_secret
+import json
+dirname = os.path.dirname(__file__)
+settings_path = pjoin(dirname, 'jupyterhub_config_private.json')
+with open(settings_path, 'r') as f:
+  s = json.load(f)
+a.oauth_callback_url = '%s:%d/hub/oauth_callback' % (s['url'], s['port'])
+a.client_id = s['client_id']
+a.client_secret = s['client_secret']
 a.create_system_users = True
-a.admin_users = admin_users
-a.whitelist = white_list
+a.admin_users = s['admin_users']
+a.whitelist = s['white_list']
